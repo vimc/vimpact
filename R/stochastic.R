bc118a935a9f489a5077d137681d4946f7fb969c
Robert Ashton <r.ashton@imperial.ac.uk>
2020-07-28 14:39:27 +0100

    Add filters to query and write docs

--- R/stochastic.R
+++ R/stochastic.R
@@ -1,6 +1,26 @@
+#' Get data from stochastic tables with specified grouping and filters
+#' 
+#' Can query from cross_all, cross_under5, cohort_all and cohort_under5
+#'
+#' @param annex Connection to annex db
+#' @param table One of cross_all, cross_under5, cohort_all or cohort_under5
+#' @param groups Categories to group by for aggregating in query
+#' @param filters Filters to apply before aggregation
+#'
+#' @return Mean, 0.025 and 0.975 quantiles for deaths_default, deaths_novac.
+#' deaths_impact, dalys_default, dalys_novac, dalys_impact with specified
+#' groupings.
+#' @export
 get_stochastic_data <- function(annex, table, 
-                                groups = c("disease", "country", "year")) {
+                                groups = c("disease", "country", "year"),
+                                filters = NULL) {
+  if (!(table %in% c("cross_all", "cross_under5", "cohort_all",
+                     "cohort_under5"))) {
+    stop(paste0("Table must be one of cross_all, cross_under5, cohort_all or",
+                " cohort_under5 got ", table))
+  }
   groups_str <- paste(groups, collapse = ", ")
+  where_clause <- build_where(filters)
   DBI::dbGetQuery(annex, sprintf("
   SELECT %s,
     avg(deaths_default) as deaths_default_mid,
@@ -22,5 +42,37 @@ get_stochastic_data <- function(annex, table,
     percentile_cont(0.975) WITHIN GROUP (ORDER BY dalys_novac) AS dalys_novac_hi,
     percentile_cont(0.975) WITHIN GROUP (ORDER BY dalys_impact) AS dalys_impact_hi
   FROM %s
-  GROUP BY %s;", groups_str, table, groups_str))
+  %s
+  GROUP BY %s;", groups_str, table, where_clause, groups_str))
+}
+
+build_where <- function(filters) {
+  single_filter_to_string <- function(filter_name, filter_values) {
+    filters <- vcapply(filter_values, function(value) {
+      if (is.na(value) || is.null(value)) {
+        value_text <- "IS NULL"
+      } else if (is.character(value)) {
+        value_text <- paste0("= '", value, "'")
+      } else {
+        value_text <- paste0("= ", value)
+      }
+      paste0("(", filter_name, " ", value_text, ")")
+    })
+    paste(filters, collapse = " OR ")
+  }
+  if (!is.null(filters)) {
+    where <- Map(single_filter_to_string, names(filters), filters)
+    if (length(where) > 1) {
+      ## If filter on more than 1 column then separate each column condition
+      ## with brackets. This is so e.g. we get (A1 ∪ A2) ∩ (B1 ∪ B2) opposed to
+      ## A1 ∪ A2 ∩ B1 ∪ B2
+      where <- vcapply(where, function(x) {
+        paste0("(", x, ")")
+      })
+    }
+    where_clause <- paste("WHERE", paste(where, collapse = " AND\n"))
+  } else {
+    where_clause <- ""
+  }
+  where_clause
 }
--- tests/testthat/test-stochastic.R
+++ tests/testthat/test-stochastic.R
@@ -79,3 +79,46 @@ test_that("get_stochastic can set groups", {
       "deaths_impact_hi", "dalys_default_hi", "dalys_novac_hi", 
       "dalys_impact_hi"))
 })
+
+test_that("get_stochastic can filter before aggregating", {
+  con <- get_test_connection()
+  
+  data <- get_stochastic_data(con, "cross_all",
+                              filters = list(
+                                disease = "HepB",
+                                year = c(2001, 2002, 2003)
+                              ))
+  expect_equal(nrow(data), 6)
+  expect_setequal(
+    colnames(data), 
+    c("disease", "country", "year", "deaths_default_mid", "deaths_novac_mid",
+      "deaths_impact_mid", "dalys_default_mid", "dalys_novac_mid",
+      "dalys_impact_mid", "deaths_default_lo", "deaths_novac_lo", 
+      "deaths_impact_lo", "dalys_default_lo", "dalys_novac_lo", 
+      "dalys_impact_lo", "deaths_default_hi", "deaths_novac_hi", 
+      "deaths_impact_hi", "dalys_default_hi", "dalys_novac_hi", 
+      "dalys_impact_hi"))
+  expect_setequal(data$disease, "HepB")
+  expect_setequal(data$country, c("AFG", "NGA"))
+  expect_setequal(data$year, c(2001, 2002, 2003))
+})
+
+test_that("where clause build correctly", {
+  filters <- list(
+    disease = "HepB",
+    year = c(2001, 2002, 2003)
+  )
+  expect_equal(build_where(filters), paste0(
+    "WHERE (disease = 'HepB') AND\n",
+    "(year = 2001) OR (year = 2002) OR (year = 2003)"))
+
+  filters <- list(
+    test = TRUE
+  )
+  expect_equal(build_where(filters), "WHERE (test = TRUE)")
+
+  filters <- list(
+    test = NA
+  )
+  expect_equal(build_where(filters), "WHERE (test IS NULL)")
+})
