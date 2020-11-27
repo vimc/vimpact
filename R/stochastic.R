ae2daf431ee0e555a7a65fd8bf74d0152f16c734
Robert Ashton <r.ashton@imperial.ac.uk>
2020-11-27 18:22:47 +0000

    Add helper function for getting stochastic summary data aggregated over year groups

--- R/stochastic.R
+++ R/stochastic.R
@@ -12,10 +12,11 @@
 #' groupings.
 #' @export
 fetch_stochastic_data <- function(annex, table,
-                                groups = c("disease", "country", "year"),
-                                filters = NULL) {
+                                  groups = c("disease", "country", "year"),
+                                  filters = NULL) {
   if (!(table %in% c("cross_all", "cross_under5", "cohort_all",
-                     "cohort_under5"))) {
+                     "cohort_under5", "cross_all_2019", "cross_under5_2019",
+                     "cohort_all_2019", "cohort_under5_2019"))) {
     stop(paste0("Table must be one of cross_all, cross_under5, cohort_all or",
                 " cohort_under5 got ", table, "."))
   }
@@ -44,6 +45,117 @@ fetch_stochastic_data <- function(annex, table,
   FROM %s
   %s
   GROUP BY %s;", groups_str, table, where_clause, groups_str))
+}
+
+#' Get data from stochastic tables with specified grouping and filters
+#'
+#' This will retrieve mean, 0.025 and 0.975 quantiles from cross_all_2019,
+#' cross_under5_2019, cohort_all_2019 and cohort_under5_2019. You can pass
+#' a set of year groups to initially aggregate over a range of years. Pass
+#' individual years to get mean and quantiles for a year alone.
+#'
+#' @param annex Connection to annex db
+#' @param table One of cross_all, cross_under5, cohort_all or cohort_under5
+#' @param groups Categories to group by for aggregating in query, can be any
+#' combination of disease and/or country
+#' @param filters Filters to apply before aggregation
+#' @param year_groups List of year groups to sum over before calculating
+#' mean and quantiles. This will all years within range from min & max
+#' of each year group. Note that passing a range of years wider than
+#' the data itself will only aggregate over the years for which there is data
+#' available
+#' @param include_proprotion_averted If TRUE then calculates mean and quantiles
+#' for proportion_deaths_averted = deaths_impact / deaths_novac and
+#' for proportion_dalys_averted = dalys_impact / dalys_novac
+#'
+#' @return Mean, 0.025 and 0.975 quantiles for deaths_default, deaths_novac.
+#' deaths_impact, dalys_default, dalys_novac, dalys_impact with specified
+#' groupings.
+#' @export
+fetch_stochastic_data_year_groups <- function(
+  annex, table, groups = c("disease", "country"), filters = NULL,
+  year_groups = list(c(2000:2019)),
+  include_proprotion_averted = FALSE) {
+  if (!(table %in% c("cross_all_2019", "cross_under5_2019", "cohort_all_2019",
+                     "cohort_under5_2019"))) {
+    stop(paste0("Table must be one of cross_all_2019, cross_under5_2019, cohort_all_2019 or",
+                " cohort_under5_2019 got ", table, "."))
+  }
+  if ("year" %in% names(filters)) {
+    stop("Can't filter year as year used in grouping")
+  }
+  groups_str <- paste(groups, collapse = ", ")
+  where_clause <- build_where(filters)
+  years_clause <- build_years(year_groups)
+  if (isTRUE(include_proprotion_averted)) {
+    averted_avg <- paste0(
+      "avg(deaths_impact / deaths_novac) as proportion_deaths_averted_mean,\n",
+      "avg(dalys_impact / dalys_novac) as proportion_dalys_averted_mean")
+    averted_q1 <- paste0(
+      "percentile_cont(0.025) WITHIN GROUP (ORDER BY deaths_impact / deaths_novac) AS proportion_deaths_averted_q1,\n",
+      "percentile_cont(0.025) WITHIN GROUP (ORDER BY dalys_impact / dalys_novac) AS proportion_dalys_averted_q1")
+    averted_q3 <-  paste0(",\n",
+      "percentile_cont(0.975) WITHIN GROUP (ORDER BY deaths_impact / deaths_novac) AS proportion_deaths_averted_q3,\n",
+      "percentile_cont(0.975) WITHIN GROUP (ORDER BY dalys_impact / dalys_novac) AS proportion_dalys_averted_q3")
+  } else {
+    averted_avg <- ""
+    averted_q1 <- ""
+    averted_q3 <- ""
+  }
+
+  DBI::dbGetQuery(annex, sprintf("
+  with sums AS (
+    SELECT
+      %s,
+      periods.start_year as start_time,
+      periods.end_year as end_time,
+      SUM(deaths_default) as deaths_default,
+      SUM(deaths_novac) as deaths_novac,
+      SUM(deaths_impact) as deaths_impact,
+      SUM(dalys_default) as dalys_default,
+      SUM(dalys_novac) as dalys_novac,
+      SUM(dalys_impact) as dalys_impact
+    FROM
+      %s
+    JOIN (%s) as periods
+    ON
+      %s.year BETWEEN periods.start_year AND periods.end_year
+    %s
+    GROUP BY
+      %s, run_id, stochastic_file_id,
+      periods.start_year, periods.end_year)
+
+    SELECT
+      %s,
+      start_time,
+      end_time,
+      avg(deaths_default) as deaths_default_mean,
+      avg(deaths_novac) as deaths_novac_mean,
+      avg(deaths_impact) as deaths_impact_mean,
+      avg(dalys_default) as dalys_default_mean,
+      avg(dalys_novac) as dalys_novac_mean,
+      avg(dalys_impact) as dalys_impact_mean,
+      %s,
+      percentile_cont(0.025) WITHIN GROUP (ORDER BY deaths_default) AS deaths_default_q1,
+      percentile_cont(0.025) WITHIN GROUP (ORDER BY deaths_novac) AS deaths_novac_q1,
+      percentile_cont(0.025) WITHIN GROUP (ORDER BY deaths_impact) AS deaths_impact_q1,
+      percentile_cont(0.025) WITHIN GROUP (ORDER BY dalys_default) AS dalys_default_q1,
+      percentile_cont(0.025) WITHIN GROUP (ORDER BY dalys_novac) AS dalys_novac_q1,
+      percentile_cont(0.025) WITHIN GROUP (ORDER BY dalys_impact) AS dalys_impact_q1,
+      %s,
+      percentile_cont(0.975) WITHIN GROUP (ORDER BY deaths_default) AS deaths_default_q3,
+      percentile_cont(0.975) WITHIN GROUP (ORDER BY deaths_novac) AS deaths_novac_q3,
+      percentile_cont(0.975) WITHIN GROUP (ORDER BY deaths_impact) AS deaths_impact_q3,
+      percentile_cont(0.975) WITHIN GROUP (ORDER BY dalys_default) AS dalys_default_q3,
+      percentile_cont(0.975) WITHIN GROUP (ORDER BY dalys_novac) AS dalys_novac_q3,
+      percentile_cont(0.975) WITHIN GROUP (ORDER BY dalys_impact) AS dalys_impact_q3
+      %s
+    FROM
+      sums
+    GROUP BY
+      %s, start_time, end_time",
+    groups_str, table, years_clause, table, where_clause, groups_str,
+    groups_str, averted_avg, averted_q1, averted_q3, groups_str))
 }
 
 build_where <- function(filters) {
@@ -75,4 +187,14 @@ build_where <- function(filters) {
     where_clause <- ""
   }
   where_clause
+}
+
+build_years <- function(year_groups) {
+  if (is.null(year_groups)) {
+    return("")
+  }
+  year_clauses <- lapply(year_groups, function(group) {
+    sprintf("SELECT %s AS start_year, %s AS end_year", min(group), max(group))
+  })
+  paste(year_clauses, collapse = " UNION ALL\n")
 }
--- tests/testthat/test-stochastic.R
+++ tests/testthat/test-stochastic.R
@@ -128,3 +128,238 @@ test_that("where clause build correctly", {
   )
   expect_equal(build_where(filters), "WHERE (test IS NULL)")
 })
+
+test_that("build years clause builds correctly", {
+  years <- list(2000:2003, 2004, 2005, c(2010, 2013))
+  clause <- build_years(years)
+  expect_equal(clause, paste0(
+               "SELECT 2000 AS start_year, 2003 AS end_year UNION ALL\n",
+               "SELECT 2004 AS start_year, 2004 AS end_year UNION ALL\n",
+               "SELECT 2005 AS start_year, 2005 AS end_year UNION ALL\n",
+               "SELECT 2010 AS start_year, 2013 AS end_year"))
+})
+
+test_that("can pull stochastic data means for year groups", {
+  con <- get_test_connection()
+  data <- fetch_stochastic_data_year_groups(con, "cross_all_2019")
+
+  ## Aggregating over all years, 2 countries and 2 diseases in test data
+  ## gives 4 rows of data
+  expect_equal(nrow(data), 4)
+  expect_setequal(
+    colnames(data),
+    c("disease", "country", "start_time", "end_time", "deaths_default_mean",
+      "deaths_novac_mean", "deaths_impact_mean", "dalys_default_mean",
+      "dalys_novac_mean", "dalys_impact_mean", "deaths_default_q1",
+      "deaths_novac_q1", "deaths_impact_q1", "dalys_default_q1",
+      "dalys_novac_q1", "dalys_impact_q1", "deaths_default_q3",
+      "deaths_novac_q3", "deaths_impact_q3", "dalys_default_q3",
+      "dalys_novac_q3", "dalys_impact_q3"))
+  expect_true(all(data$deaths_default_q1 < data$deaths_default_mean))
+  expect_true(all(data$deaths_default_mean < data$deaths_default_q3))
+  expect_true(all(data$deaths_novac_q1 < data$deaths_novac_mean))
+  expect_true(all(data$deaths_novac_mean < data$deaths_novac_q3))
+  expect_true(all(data$deaths_impact_q1 < data$deaths_impact_mean))
+  expect_true(all(data$deaths_impact_mean < data$deaths_impact_q3))
+  expect_true(all(data$dalys_default_q1 < data$dalys_default_mean))
+  expect_true(all(data$dalys_default_mean < data$dalys_default_q3))
+  expect_true(all(data$dalys_novac_q1 < data$dalys_novac_mean))
+  expect_true(all(data$dalys_novac_mean < data$dalys_novac_q3))
+  expect_true(all(data$dalys_impact_q1 < data$dalys_impact_mean))
+  expect_true(all(data$dalys_impact_mean < data$dalys_impact_q3))
+  expect_true(all(data$start_time == 2000))
+  expect_true(all(data$end_time == 2019))
+
+  data <- fetch_stochastic_data_year_groups(
+    con, "cross_under5_2019", year_groups = list(2001:2005, 2006, 2007:2015))
+  ## 2 countries, 2 diseases, 3 year groups
+  expect_equal(nrow(data), 12)
+  expect_setequal(
+    colnames(data),
+    c("disease", "country", "start_time", "end_time", "deaths_default_mean",
+      "deaths_novac_mean", "deaths_impact_mean", "dalys_default_mean",
+      "dalys_novac_mean", "dalys_impact_mean", "deaths_default_q1",
+      "deaths_novac_q1", "deaths_impact_q1", "dalys_default_q1",
+      "dalys_novac_q1", "dalys_impact_q1", "deaths_default_q3",
+      "deaths_novac_q3", "deaths_impact_q3", "dalys_default_q3",
+      "dalys_novac_q3", "dalys_impact_q3"))
+  expect_equal()
+
+  data <- fetch_stochastic_data_year_groups(con, "cohort_all_2019",
+                                            year_groups = 2001:2015)
+  ## 2 countries, 2 diseases, 15 years
+  expect_equal(nrow(data), 60)
+  expect_setequal(
+    colnames(data),
+    c("disease", "country", "start_time", "end_time", "deaths_default_mean",
+      "deaths_novac_mean", "deaths_impact_mean", "dalys_default_mean",
+      "dalys_novac_mean", "dalys_impact_mean", "deaths_default_q1",
+      "deaths_novac_q1", "deaths_impact_q1", "dalys_default_q1",
+      "dalys_novac_q1", "dalys_impact_q1", "deaths_default_q3",
+      "deaths_novac_q3", "deaths_impact_q3", "dalys_default_q3",
+      "dalys_novac_q3", "dalys_impact_q3"))
+
+  data <- fetch_stochastic_data_year_groups(con, "cohort_under5_2019",
+                                            year_groups = 2001:2020)
+  ## 2 countries, 2 diseases, 20 years but only 5 years present in data
+  ## maybe we should error if years outside db range?
+  expect_equal(nrow(data), 60)
+  expect_setequal(
+    colnames(data),
+    c("disease", "country", "start_time", "end_time", "deaths_default_mean",
+      "deaths_novac_mean", "deaths_impact_mean", "dalys_default_mean",
+      "dalys_novac_mean", "dalys_impact_mean", "deaths_default_q1",
+      "deaths_novac_q1", "deaths_impact_q1", "dalys_default_q1",
+      "dalys_novac_q1", "dalys_impact_q1", "deaths_default_q3",
+      "deaths_novac_q3", "deaths_impact_q3", "dalys_default_q3",
+      "dalys_novac_q3", "dalys_impact_q3"))
+})
+
+test_that("year group summarised stochastics can filter", {
+  con <- get_test_connection()
+  data <- fetch_stochastic_data_year_groups(con, "cross_all_2019",
+                                            filters = list(disease = "HepB"))
+  ## 2 countries, 1 diseases, 1 year group
+  expect_equal(nrow(data), 2)
+  expect_setequal(
+    colnames(data),
+    c("disease", "country", "start_time", "end_time", "deaths_default_mean",
+      "deaths_novac_mean", "deaths_impact_mean", "dalys_default_mean",
+      "dalys_novac_mean", "dalys_impact_mean", "deaths_default_q1",
+      "deaths_novac_q1", "deaths_impact_q1", "dalys_default_q1",
+      "dalys_novac_q1", "dalys_impact_q1", "deaths_default_q3",
+      "deaths_novac_q3", "deaths_impact_q3", "dalys_default_q3",
+      "dalys_novac_q3", "dalys_impact_q3"))
+  expect_true(all(data$disease == "HepB"))
+})
+
+test_that("year group summarised stochastics can aggregate over groups", {
+  con <- get_test_connection()
+  data <- fetch_stochastic_data_year_groups(con, "cross_all_2019",
+                                            groups = "disease")
+  ## 2 diseases, 1 year group
+  expect_equal(nrow(data), 2)
+  expect_true(!("country" %in% colnames(data)))
+})
+
+test_that("year group summarised stochastics can get proportion averted", {
+  con <- get_test_connection()
+  data <- fetch_stochastic_data_year_groups(con, "cross_all_2019",
+                                            include_proprotion_averted = TRUE)
+  ## 2 diseases, 2 countries, 1 year group
+  expect_equal(nrow(data), 4)
+  expect_setequal(
+    colnames(data),
+    c("disease", "country", "start_time", "end_time", "deaths_default_mean",
+      "deaths_novac_mean", "deaths_impact_mean", "dalys_default_mean",
+      "dalys_novac_mean", "dalys_impact_mean", "proportion_deaths_averted_mean",
+      "proportion_dalys_averted_mean", "deaths_default_q1",
+      "deaths_novac_q1", "deaths_impact_q1", "dalys_default_q1",
+      "dalys_novac_q1", "dalys_impact_q1", "proportion_deaths_averted_q1",
+      "proportion_dalys_averted_q1", "deaths_default_q3",
+      "deaths_novac_q3", "deaths_impact_q3", "dalys_default_q3",
+      "dalys_novac_q3", "dalys_impact_q3",  "proportion_deaths_averted_q3",
+      "proportion_dalys_averted_q3"))
+  expect_true(!any(is.na(data$proportion_deaths_averted_mean)))
+  expect_true(all(is.finite(data$proportion_deaths_averted_mean)))
+  expect_true(!any(is.na(data$proportion_dalys_averted_mean)))
+  expect_true(all(is.finite(data$proportion_dalys_averted_mean)))
+  expect_true(!any(is.na(data$proportion_deaths_averted_q1)))
+  expect_true(all(is.finite(data$proportion_deaths_averted_q1)))
+  expect_true(!any(is.na(data$proportion_dalys_averted_q1)))
+  expect_true(all(is.finite(data$proportion_dalys_averted_q1)))
+  expect_true(!any(is.na(data$proportion_deaths_averted_q3)))
+  expect_true(all(is.finite(data$proportion_deaths_averted_q3)))
+  expect_true(!any(is.na(data$proportion_dalys_averted_q3)))
+  expect_true(all(is.finite(data$proportion_dalys_averted_q3)))
+})
+
+test_that("proportion averted deals with missing data by ignoring", {
+  con <- get_test_connection()
+
+  ## Try 1 empty group (so summary table has an empty entry)
+  updated <- DBI::dbExecute(con, "UPDATE cross_all_2019
+                 SET deaths_impact = NULL
+                 WHERE disease = 'HepB'
+                 AND country = 'AFG'
+                 AND year = 2001")
+  expect_equal(updated, 10)
+
+  data <- fetch_stochastic_data_year_groups(con, "cross_all_2019",
+                                            include_proprotion_averted = TRUE)
+  ## 2 diseases, 2 countries, 1 year group
+  expect_equal(nrow(data), 4)
+  expect_true(!any(is.na(data$proportion_deaths_averted_mean)))
+  expect_true(all(is.finite(data$proportion_deaths_averted_mean)))
+  expect_true(!any(is.na(data$proportion_dalys_averted_mean)))
+  expect_true(all(is.finite(data$proportion_dalys_averted_mean)))
+  expect_true(!any(is.na(data$proportion_deaths_averted_q1)))
+  expect_true(all(is.finite(data$proportion_deaths_averted_q1)))
+  expect_true(!any(is.na(data$proportion_dalys_averted_q1)))
+  expect_true(all(is.finite(data$proportion_dalys_averted_q1)))
+  expect_true(!any(is.na(data$proportion_deaths_averted_q3)))
+  expect_true(all(is.finite(data$proportion_deaths_averted_q3)))
+  expect_true(!any(is.na(data$proportion_dalys_averted_q3)))
+  expect_true(all(is.finite(data$proportion_dalys_averted_q3)))
+
+  ## Empty denominator will return NA
+  data <- fetch_stochastic_data_year_groups(con, "cross_all_2019",
+                                            include_proprotion_averted = TRUE,
+                                            year_groups = list(2001, 2002:2015))
+  ## 2 diseases, 2 countries, 2 year group
+  expect_equal(nrow(data), 8)
+  expect_equal(data$proportion_deaths_averted_mean[1], NA_real_)
+  expect_true(!any(is.na(data$proportion_deaths_averted_mean[2:8])))
+  expect_true(all(is.finite(data$proportion_deaths_averted_mean[2:8])))
+  expect_true(!any(is.na(data$proportion_dalys_averted_mean)))
+  expect_true(all(is.finite(data$proportion_dalys_averted_mean)))
+  expect_equal(data$proportion_deaths_averted_q1[1], NA_real_)
+  expect_true(!any(is.na(data$proportion_deaths_averted_q1[2:8])))
+  expect_true(all(is.finite(data$proportion_deaths_averted_q1[2:8])))
+  expect_true(!any(is.na(data$proportion_dalys_averted_q1)))
+  expect_true(all(is.finite(data$proportion_dalys_averted_q1)))
+  expect_equal(data$proportion_deaths_averted_q3[1], NA_real_)
+  expect_true(!any(is.na(data$proportion_deaths_averted_q3[2:8])))
+  expect_true(all(is.finite(data$proportion_deaths_averted_q3[2:8])))
+  expect_true(!any(is.na(data$proportion_dalys_averted_q3)))
+  expect_true(all(is.finite(data$proportion_dalys_averted_q3)))
+})
+
+test_that("using a single year group returns same as fetch_stochastic_data", {
+  con <- get_test_connection()
+  data_grouped <- fetch_stochastic_data_year_groups(con, "cross_all_2019",
+                                            year_groups = as.list(2001:2015))
+  data <- fetch_stochastic_data(con, "cross_all_2019",
+                               groups = c("disease", "country", "year"))
+
+  ## Align format
+  nms <- names(data_grouped)
+  nms[which(nms == "start_time")] <- "year"
+  names(data_grouped) <- nms
+  data_grouped <- data_grouped[ , colnames(data_grouped) != "end_time"]
+
+  expect_equivalent(data_grouped, data)
+
+  ## and filters work
+  filters <- list(
+    disease = "HepB"
+  )
+  data_grouped <- fetch_stochastic_data_year_groups(
+    con, "cross_all_2019", year_groups = as.list(2001:2015), filters = filters)
+
+  data <- fetch_stochastic_data(con, "cross_all_2019", filters = filters)
+
+  ## Align format
+  nms <- names(data_grouped)
+  nms[which(nms == "start_time")] <- "year"
+  names(data_grouped) <- nms
+  data_grouped <- data_grouped[ , colnames(data_grouped) != "end_time"]
+
+  expect_equivalent(data_grouped, data)
+})
+
+test_that("filtering on year doesn't work", {
+
+})
+
+## Test other errors too
