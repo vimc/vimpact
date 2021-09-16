69282ff9614b5e83f8d6eff2f30a6e555f755bca
Robert Ashton <r.ashton@imperial.ac.uk>
2021-09-16 17:11:27 +0100

    Add test for impact by birth year

--- tests/testthat/test-vimpact.R
+++ tests/testthat/test-vimpact.R
@@ -44,7 +44,39 @@ test_that("can calculate impact by calendar year from db", {
     burden_outcome = "hepb_deaths_acute;hepb_cases_acute_severe")
   t <- tempfile(fileext = ".csv")
   write.csv(recipe, t, row.names = FALSE)
-  meta <- get_meta_from_recipe(default_recipe = FALSE, recipe = t, con = con)
+  meta <- get_meta_from_recipe(default_recipe = FALSE, recipe = t, method = "method0", con = con)
+  old_impact <- get_raw_impact_details(con, meta, "dalys")
+  country <- dplyr::tbl(con, "country")
+  old_impact <- old_impact %>%
+    dplyr::left_join(country, by = c("country" = "nid"), copy = TRUE) %>%
+    dplyr::select(-country, -name) %>%
+    dplyr::select(country = id, burden_outcome, year = time, impact = value) %>%
+    dplyr::arrange(country, year)
+
+  expect_equal(impact, old_impact)
+})
+
+test_that("can calculate impact by birth year from db", {
+  con <- test_montagu_readonly_connection()
+  impact <- calculate_impact(
+    con, "birth_year", touchstone = "201710gavi-5",
+    modelling_group = "CDA-Razavi",  disease = "HepB",
+    focal_scenario_type = "default", focal_vaccine_delivery = list(
+      list(vaccine = "HepB_BD", activity_type = "routine"),
+      list(vaccine = "HepB", activity_type = "routine")
+    ),
+    baseline_scenario_type = "novac",
+    burden_outcomes = c("dalys"))
+
+  recipe <- data.frame(
+    touchstone = "201710gavi-5",
+    modelling_group = "CDA-Razavi", disease = "HepB",
+    focal = "default:HepB_BD-routine;HepB-routine",
+    baseline = "novac",
+    burden_outcome = "hepb_deaths_acute;hepb_cases_acute_severe")
+  t <- tempfile(fileext = ".csv")
+  write.csv(recipe, t, row.names = FALSE)
+  meta <- get_meta_from_recipe(default_recipe = FALSE, recipe = t, method = "method1", con = con)
   old_impact <- get_raw_impact_details(con, meta, "dalys")
   country <- dplyr::tbl(con, "country")
   old_impact <- old_impact %>%
